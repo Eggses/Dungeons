d1cb5ec4c5212c2e3174685a04d5eb454b7134ef
Eggses <194306815+Eggses@users.noreply.github.com>
2025-12-11 11:44:42 +1100

    Moved file logic out of DungeonInstance and into DungeonCreation.

--- src/main/java/me/Eggses/dungeons/dungeon/DungeonCreation.java
+++ src/main/java/me/Eggses/dungeons/dungeon/DungeonCreation.java
@@ -1,22 +1,116 @@
 package me.Eggses.dungeons.dungeon;
 
+import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.WorldCreator;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
 import java.util.function.Consumer;
 
 public class DungeonCreation {
 
-    private World createdWorld;
+    private static final Set<String> FILES_TO_IGNORE = Set.of("session.lock", "uid.dat");
 
+    private final JavaPlugin plugin;
+    private final String fileNameOfTemplate;
+    private final String fileNameOfNewInstance;
+    private final Consumer<World> onCreation;
+    private final Consumer<Exception> onFailure;
 
     public DungeonCreation(JavaPlugin plugin,
                            String fileNameOfTemplate,
                            String fileNameOfNewInstance,
                            Consumer<World> onCreation,
-                           Consumer<IOException> onFailure) {
+                           Consumer<Exception> onFailure) {
 
+        this.plugin = plugin;
+        this.fileNameOfTemplate = fileNameOfTemplate;
+        this.fileNameOfNewInstance = fileNameOfNewInstance;
+        this.onCreation = onCreation;
+        this.onFailure = onFailure;
     }
 
+    public void createInstance() {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, this::createWorld);
+    }
+
+    private void createWorld() {
+
+        File serverFolder = Bukkit.getWorldContainer();
+
+        File sourceTemplateFolder = new File(serverFolder, fileNameOfTemplate);
+        if (!(sourceTemplateFolder.exists() && sourceTemplateFolder.isDirectory())) {
+            failure(new FileNotFoundException(
+                    "Dungeon Template Folder not found or not a directory: " + sourceTemplateFolder.getPath() + "."));
+            return;
+        }
+
+        File destinationOfInstance = new File(serverFolder, fileNameOfNewInstance);
+        if (destinationOfInstance.exists()) {
+            failure(new FileAlreadyExistsException(fileNameOfNewInstance));
+            return;
+        }
+
+        try {
+            copyFolderBFS(sourceTemplateFolder.toPath(), destinationOfInstance.toPath());
+        } catch (IOException e) {
+            failure(e);
+            return;
+        }
+
+        Bukkit.getScheduler().runTask(plugin, () -> {
+            WorldCreator worldCreator = new WorldCreator(destinationOfInstance.getName());
+            World world = worldCreator.createWorld();
+
+            if (world != null) {
+                onCreation.accept(world);
+            } else {
+                failure(new IllegalArgumentException(
+                        "WorldCreator returned null for " + destinationOfInstance.getName() + "."));
+            }
+        });
+    }
+
+    private void copyFolderBFS(Path sourceFolder, Path destinationFolder) throws IOException {
+
+        Queue<File> queueOfFolders = new LinkedList<>();
+        Files.createDirectories(destinationFolder); // Makes a Folder WITH that path, NOT in that path!
+        queueOfFolders.add(sourceFolder.toFile());
+
+        while (!queueOfFolders.isEmpty()) {
+
+            File folder = queueOfFolders.poll();
+            File[] files = folder.listFiles();
+            if (files == null) continue;
+
+            for (File file : files) {
+
+                Path childPath = file.toPath();
+                Path relativePathInFolder = sourceFolder.relativize(childPath);
+                Path specificDestinationPath = destinationFolder.resolve(relativePathInFolder);
+
+                if (file.isDirectory()) {
+                    Files.createDirectories(specificDestinationPath);
+                    queueOfFolders.offer(file);
+                } else {
+                    if (FILES_TO_IGNORE.contains(file.getName())) continue;
+                    Files.copy(childPath, specificDestinationPath, StandardCopyOption.REPLACE_EXISTING);
+                }
+            }
+        }
+    }
+
+    private void failure(Exception exception) {
+        Bukkit.getScheduler().runTask(plugin, () -> onFailure.accept(exception));
+    }
 }
--- src/main/java/me/Eggses/dungeons/dungeon/DungeonInstance.java
+++ src/main/java/me/Eggses/dungeons/dungeon/DungeonInstance.java
@@ -2,151 +2,40 @@ package me.Eggses.dungeons.dungeon;
 
 import org.bukkit.Bukkit;
 import org.bukkit.World;
-import org.bukkit.WorldCreator;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scheduler.BukkitTask;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.*;
 import java.util.logging.Level;
 
 public abstract class DungeonInstance {
 
-    private static final Set<String> FILES_TO_IGNORE = Set.of("session.lock", "uid.dat");
-    private final Set<BukkitTask> tasks = new HashSet<>();
-
-    private boolean created = false;
+    private static final int PORTAL_OPEN_DURATION_TICKS = 120 * 20;
 
     private final JavaPlugin plugin;
     private World dungeonWorld = null;
 
-
-    set keep inventory, no natural spawning etc stuff? maybe in start dungoen method
-
-    public DungeonInstance(JavaPlugin plugin, String dungeonTemplateName) {
+    public DungeonInstance(JavaPlugin plugin, DungeonManager dungeonManager, String dungeonTemplateFileName) {
 
         this.plugin = plugin;
 
-        File serverFolder = Bukkit.getWorldContainer();
-        createWorld(serverFolder, dungeonTemplateName);
-    }
-
-    private void createWorld(File serverFolder, String dungeonTemplateName) {
-
-        BukkitTask makeWorldTask = new BukkitRunnable() {
-
-            @Override
-            public void run() {
-                createWorldFromTemplate(serverFolder, dungeonTemplateName);
-            }
-
-        }.runTaskAsynchronously(plugin);
-
-        BukkitTask waitForWorldTask = new BukkitRunnable() {
-
-            final long startTime = System.currentTimeMillis();
-            final long maxAllowedTime = 120000;
-
-            @Override
-            public void run() {
-
-                if (dungeonWorld != null) {
-                    endDungeonCreationTasks();
+        DungeonCreation dungeonCreation = new DungeonCreation(
+                plugin,
+                dungeonTemplateFileName,
+                produceInstanceName(),
+                world -> {
+                    this.dungeonWorld = world;
                     startDungeon();
-                    return;
-                }
-
-                if (System.currentTimeMillis() > startTime + maxAllowedTime && dungeonWorld == null) {
-                    endDungeonCreationTasks();
-                    plugin.getLogger().severe("Could not start Dungeon: " + dungeonTemplateName + ".");
-                }
-            }
-
-        }.runTaskTimer(plugin, 20 * 10, 20);
-
-        tasks.add(makeWorldTask);
-        tasks.add(waitForWorldTask);
-    }
-
-    private void endDungeonCreationTasks() {
-        for (BukkitTask task : tasks) {
-            if (task.isCancelled()) continue;
-            task.cancel();
-        }
-        tasks.clear();
-    }
-
-    private void createWorldFromTemplate(File serverFolder, String dungeonTemplateName) {
-
-        File sourceDungeonTemplate = new File(serverFolder, dungeonTemplateName);
-
-        if (!(sourceDungeonTemplate.exists() && sourceDungeonTemplate.isDirectory())) {
-            plugin.getLogger().severe("Could not copy Dungeon Template: " + dungeonTemplateName + ".");
-            return;
-        }
-
-        File destinationOfDungeonInstance = new File(serverFolder, produceInstanceName());
-
-        try {
-            copyFolderBFS(sourceDungeonTemplate.toPath(), destinationOfDungeonInstance.toPath());
-        } catch (IOException e) {
-            plugin.getLogger().log(Level.SEVERE, "Could not copy Dungeon Template: " + dungeonTemplateName, e);
-            return;
-        }
-
-        Bukkit.getScheduler().runTask(plugin, () -> {
-            WorldCreator worldCreator = new WorldCreator(destinationOfDungeonInstance.getName());
-            World world = worldCreator.createWorld();
-
-            if (world == null) {
-                plugin.getLogger().severe("Could not load World with Dungeon Template: " + dungeonTemplateName + ".");
-                return;
-            }
-
-            this.dungeonWorld = world;
-        });
-    }
-
-    private void copyFolderBFS(Path sourceFolder, Path destinationFolder) throws IOException {
-
-        Queue<File> queueOfFolders = new LinkedList<>();
-        Files.createDirectories(destinationFolder); // Makes a Folder WITH that path, NOT in that path!
-        queueOfFolders.add(sourceFolder.toFile());
-
-        while (!queueOfFolders.isEmpty()) {
-
-            File folder = queueOfFolders.poll();
-            File[] files = folder.listFiles();
-            if (files == null) continue;
-
-            for (File file : files) {
-
-                Path childPath = file.toPath();
-                Path relativePathInFolder = sourceFolder.relativize(childPath);
-                Path specificDestinationPath = destinationFolder.resolve(relativePathInFolder);
-
-                if (file.isDirectory()) {
-                    Files.createDirectories(specificDestinationPath);
-                    queueOfFolders.offer(file);
-                } else {
-                    if (FILES_TO_IGNORE.contains(file.getName())) continue;
-                    Files.copy(childPath, specificDestinationPath, StandardCopyOption.REPLACE_EXISTING);
-                }
-            }
-        }
+                },
+                exception -> {
+                    plugin.getLogger().log(Level.SEVERE, "Error In Creation", exception);
+                });
+        dungeonCreation.createInstance();
     }
 
     private void startDungeon() {
-        this.created = true;
         openPortal();
-        Bukkit.getScheduler().runTaskLater(plugin, this::closePortal, 20 * 120);
+        Bukkit.getScheduler().runTaskLater(plugin, this::closePortal, PORTAL_OPEN_DURATION_TICKS);
     }
 
     public abstract void openPortal();
@@ -163,8 +52,13 @@ public abstract class DungeonInstance {
         return dungeonWorld.getPlayers().contains(player);
     }
 
+    /*
     public boolean isInNormalWorldPortalRoom() {
         return false;
+
+        set keep inventory, no natural spawning etc stuff? maybe in start dungoen method
+
+
         this will be like the checking to work out
                 like if someone is in a region
                 same thing... store 2 points
@@ -200,4 +94,6 @@ public abstract class DungeonInstance {
 
     and then the keysotne has a can I enter Dungeon button? click it it says Yes, all good
             or No. due to Item: SADDLE, Item:Bundle_white or Item: Bundle_red etc.
+
+     */
 }
